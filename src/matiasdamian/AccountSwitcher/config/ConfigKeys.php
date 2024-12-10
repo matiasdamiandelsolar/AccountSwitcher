c742283c2f9a5ffa6bb5f82a7c2dd4873fdd0c97
Matias Damian <matias@matiasdamian.com>
2024-12-10 23:57:22 +0000

    Improve config

--- src/matiasdamian/AccountSwitcher/config/ConfigKeys.php
+++ src/matiasdamian/AccountSwitcher/config/ConfigKeys.php
@@ -7,10 +7,15 @@ namespace matiasdamian\AccountSwitcher\config;
 interface ConfigKeys{
 	
 	public const TRANSFER_ON_SWITCH = "account-switch.transfer-on-switch";
+	
 	public const SERVER_IP = "account-switch.server-ip";
+	
 	public const SERVER_PORT = "account-switch.server-port";
+	
 	public const BAN_ALT_ACCOUNTS = "account-switch.ban-alts";
+	
 	public const ALLOW_UNGROUP = "groups.allow-ungroup";
+	
 	public const MAX_GROUP_SIZE = "groups.max-group-size";
 	
 }
